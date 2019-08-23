970ea6e1d91ac479b154f3207bda612a494bc4c0
Daniel Moraes <31326186+Brugnaro42@users.noreply.github.com>
2019-08-23 17:21:22 -0300

    Add files via upload

---
+++ LamfsowGame/Assets/Scripts/OrbitaElipse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitaElipse : MonoBehaviour
+{
+    float alpha, X, Y, Z;
+    public Transform centerPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElipticalTranslation();
+        transform.LookAt(centerPoint);
+    }
+
+    void ElipticalTranslation()
+    {
+        X = (0f + (10f * Mathf.Sin(Mathf.Deg2Rad * alpha)));
+        Z = (0f + (5f * Mathf.Cos(Mathf.Deg2Rad * alpha)));
+        transform.position = new Vector3(X, 0, Z) + centerPoint.position;
+        //transform.position = new Vector2(0f + (10f * Mathf.Sin(Mathf.Deg2Rad * alpha)),0f + (5f * Mathf.Cos(Mathf.Deg2Rad * alpha)));
+        alpha += 2f;//can be used as speed
+    }
+}
