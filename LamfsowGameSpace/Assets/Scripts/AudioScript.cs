765e058c30533d6d89cc60d27ae112e272eeb69f
Alexandre Russi <junior.russi2011@gmail.com>
2019-09-10 03:06:21 -0300

    Audio todas cenas - Todas cenas transicionando

--- LamfsowGameSpace/Assets/Scripts/AudioScript.cs
+++ LamfsowGameSpace/Assets/Scripts/AudioScript.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class transiçãocenas : MonoBehaviour
+public class AudioScript : MonoBehaviour
 {
+    public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
