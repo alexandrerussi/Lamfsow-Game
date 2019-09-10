728d2c710b3d880199d5a482088b82e9677a40f5
Daniel Moraes <31326186+Brugnaro42@users.noreply.github.com>
2019-09-10 09:10:42 -0300

    Update OrbitaElipse.cs

--- LamfsowGameSpace/Assets/Scripts/OrbitaElipse.cs
+++ LamfsowGameSpace/Assets/Scripts/OrbitaElipse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,12 @@ public class OrbitaElipse : MonoBehaviour
 {
     float alpha, X, Y, Z;
     public Transform centerPoint;
-    
+    public float spread;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,9 +24,9 @@ public class OrbitaElipse : MonoBehaviour
 
     void ElipticalTranslation()
     {
-        X = (0f + (10f * Mathf.Sin(Mathf.Deg2Rad * alpha)));
+        X = (0f + (10f * Mathf.Sin(Mathf.Deg2Rad * alpha)))*spread;
         Y = centerPoint.position.y;
-        Z = (5f * Mathf.Cos(Mathf.Deg2Rad * alpha));
+        Z = (5f * Mathf.Cos(Mathf.Deg2Rad * alpha)) * spread;
         transform.position = new Vector3(X, Y, Z);
         alpha += 2f;
         //print("Z: " + Z + "\n X: " + X); -> Para tirar conclusões geométricas
