13a9a9f2379ecc476e0dbe60f4d08e70e6109e83
Alexandre Russi <junior.russi2011@gmail.com>
2019-09-20 01:45:06 -0300

    Atualização Terra + Asset Store Terra

---
+++ LamfsowGameSpace/Assets/Scripts/rotacao.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotação : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    int yRot = 0, x=0, z=0;
+    // Update is called once per frame
+    void Update()
+    {
+        yRot--;
+        Vector3 rot = new Vector3(x, yRot, z);
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
